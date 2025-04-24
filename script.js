ef956bd771329a3132b04e704a48b0150dbf74ee
optimizein <optimizemediaom@gmail.com>
2025-04-24 19:39:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// You can add any interactive JavaScript code here if required.
+document.querySelector(".cta-btn").addEventListener("click", function() {
+    window.location.href = "mailto:optimizemediaom@gmail.com";
+});
